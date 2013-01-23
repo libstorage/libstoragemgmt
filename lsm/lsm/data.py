6ecb3af0d5dbe61b49cfaddbbff37e085b5ad0c0
Tony Asleson <tasleson@redhat.com>
2013-01-23 14:49:05 -0600

    Update copyright date
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- lsm/lsm/data.py
+++ lsm/lsm/data.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2012 Red Hat, Inc.
+# Copyright (C) 2011-2013 Red Hat, Inc.
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
--- test/cmdtest.py
+++ test/cmdtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2011-2012 Red Hat, Inc.
+# Copyright (C) 2011-2013 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
