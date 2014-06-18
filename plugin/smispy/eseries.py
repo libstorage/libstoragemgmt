73a23c50cd4b20c56ff7a8b3bbec5c9de3e0a72d
Gris Ge <fge@redhat.com>
2014-06-18 21:31:49 +0800

    NetApp E-series: Enable AccessGroup support.
     * Enable access_groups() support by forcing fallback mode.
     * Disable access_group_initiator_delete() support as we got a unexpected
       behaviour when testing. The whole SPC will be removed when deleting one
       initiator, this will cause data lose.
     * Only tested by access_groups() and access_group_initiator_add().
       Volume masking and unmasking is not tested due to hardware access issue.
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- plugin/smispy/eseries.py
+++ plugin/smispy/eseries.py
@@ -19,10 +19,54 @@ from smis import Smis
 import pywbem
 import time
 from lsm.plugin.smispy.smis import handle_cim_errors
-from lsm import LsmError, ErrorNumber, Capabilities
+from lsm import LsmError, ErrorNumber, Capabilities, uri_parse
 
 
 class ESeries(Smis):
+    def plugin_register(self, uri, password, timeout, flags=0):
+        """
+        The only difference we did here compare to supper method:
+            we force to be fallback mode.
+        NetApp-E support 'Masking and Mapping' profile but not expose it
+        via CIM_RegisteredProfile.
+        """
+        protocol = 'http'
+        port = Smis.IAAN_WBEM_HTTP_PORT
+        u = uri_parse(uri, ['scheme', 'netloc', 'host'], None)
+
+        if u['scheme'].lower() == 'smispy+ssl':
+            protocol = 'https'
+            port = Smis.IAAN_WBEM_HTTPS_PORT
+
+        if 'port' in u:
+            port = u['port']
+
+        # smisproxy.py already make sure namespace defined.
+        namespace = u['parameters']['namespace']
+        self.all_vendor_namespaces = [namespace]
+
+        url = "%s://%s:%s" % (protocol, u['host'], port)
+        self.system_list = None
+        if 'systems' in u['parameters']:
+            self.system_list = split(u['parameters']["systems"], ":")
+
+        self._c = pywbem.WBEMConnection(url, (u['username'], password),
+                                        namespace)
+        if "no_ssl_verify" in u["parameters"] \
+           and u["parameters"]["no_ssl_verify"] == 'yes':
+            try:
+                self._c = pywbem.WBEMConnection(
+                    url,
+                    (u['username'], password),
+                    namespace,
+                    no_verification=True)
+            except TypeError:
+                # pywbem is not holding fix from
+                # https://bugzilla.redhat.com/show_bug.cgi?id=1039801
+                pass
+
+        self.tmo = timeout
+        self.fallback_mode = True
 
     def _deal_volume_associations(self, vol, lun):
         """
@@ -121,3 +165,14 @@ class ESeries(Smis):
                 time.sleep(0.125)
         except LsmError as e:
             pass
+
+    @handle_cim_errors
+    def access_group_initiator_delete(self, access_group, init_id, flags=0):
+        """
+        When using HidePaths to remove initiator, the whole SPC will be
+        removed. Before we find a workaround for this, I would like to have
+        this method disabled as NO_SUPPORT.
+        """
+        raise LsmError(ErrorNumber.NO_SUPPORT,
+                       "SMI-S plugin does not support "
+                       "access_group_initiator_delete() against NetApp-E")
