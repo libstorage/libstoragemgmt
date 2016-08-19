73fa5bd3ff5c6e42e671ad4be4783c5569eea4c6
Tony Asleson <tasleson@redhat.com>
2016-08-19 11:29:37 -0500

    tools/lsmcli: Make py3 compatible
    Remove some previous py3 changes too.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- tools/lsmcli/__init__.py
+++ tools/lsmcli/__init__.py
@@ -1,2 +1,18 @@
-from __future__ import absolute_import
-from .cmdline import cmd_line_wrapper
+# Copyright (C) 2011-2016 Red Hat, Inc.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; If not, see <http://www.gnu.org/licenses/>.
+
+try:
+	from .cmdline import cmd_line_wrapper
+except ImportError:
+	from cmdline import cmd_line_wrapper
