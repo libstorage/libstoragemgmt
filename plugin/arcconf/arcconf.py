2750670e02332b7f645da3378f606b8ced50cb52
Sharath TS <sharath.ts@microchip.com>
2019-01-31 10:16:14 +0530

    Arcconf Plugin: Add 'pool-member-info' support
    Implementation for 'pool-member-info' API support
    
    * 'plugin_data' is now a string and the same is used to extract
      arcconf required data.
    
    * made use of _arcconf_raid_level_to_lsm() function to get
      proper raid level

--- plugin/arcconf/arcconf.py
+++ plugin/arcconf/arcconf.py
@@ -781,3 +781,55 @@ class Arcconf(IPlugin):
 
         return [raid_level, stripe_size, device_count, stripe_size,
                 full_stripe_size]
+
+    @_handle_errors
+    def pool_member_info(self, pool, flags=Client.FLAG_RSVD):
+        """
+
+        :param pool: pool id -  Pool to query
+        :param flags: Optional
+        :return: [raid_type, member_type, member_ids]
+
+        Depends on command:
+            arcconf getconfigjson <ctrlNo>
+        """
+
+        if not pool.plugin_data:
+            raise LsmError(
+                ErrorNumber.INVALID_ARGUMENT,
+                "Illegal input pool argument: missing plugin_data property")
+
+        pool_info = pool.plugin_data.split(':')
+        ctrl_id = pool_info[0]
+        array_id = int(pool_info[1])
+        consumer_array_id = None
+        raid_level = None
+        device_id = []
+
+        ctrl_info = self._arcconf_exec(['GETCONFIGJSON', ctrl_id])
+        ctrl_json_info = self._filter_cmd_output(ctrl_info)
+        device_info = ctrl_json_info['Controller']['Channel']
+        volume_info = ctrl_json_info['Controller']['LogicalDrive']
+
+        for volume in volume_info:
+            chunk_data = volume['Chunk']
+            for chunk in chunk_data:
+                # consumerArrayID in all the chunk will be same
+                consumer_array_id = chunk['consumerArrayID']
+            if consumer_array_id == array_id:
+                raid_level = volume['raidLevel']
+
+        for device in device_info:
+            if 'HardDrive'in device.keys():
+                for hard_drive in device['HardDrive']:
+                    chunk_data = hard_drive['Chunk']
+                    for chunk in chunk_data:
+                        if ('consumerArrayID' in chunk.keys()) and (
+                                chunk['consumerArrayID'] == array_id):
+                            device_id.append(
+                                str(hard_drive['serialNumber'].strip()))
+
+        lsm_raid_level = _arcconf_raid_level_to_lsm(str(raid_level))
+
+        return [lsm_raid_level, Pool.MEMBER_TYPE_DISK, device_id]
+
