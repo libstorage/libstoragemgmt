feb56754a6df51c945c854845a18460800378573
Sharath TS <sharath.ts@microchip.com>
2019-03-08 11:39:59 +0530

    code refactor for 'volume-idet-led-off'
    pluign_data is now a string
    maintained pep8 format for line width

--- plugin/arcconf/arcconf.py
+++ plugin/arcconf/arcconf.py
@@ -818,13 +818,15 @@ class Arcconf(IPlugin):
                 ErrorNumber.INVALID_ARGUMENT,
                 "Illegal input volume argument: missing plugin_data property")
 
-        volume_info = volume.plugin_data
-        ctrl_id = str(volume_info['ctrl_id'])
-        volume_id = str(volume_info['ld_id'])
+        volume_info = volume.plugin_data.split(':')
+        ctrl_id = str(volume_info[6])
+        volume_id = str(volume_info[4])
 
         try:
-            self._arcconf_exec(['IDENTIFY', ctrl_id, 'LOGICALDRIVE', volume_id, 'STOP'], flag_force=True)
+            self._arcconf_exec(['IDENTIFY', ctrl_id, 'LOGICALDRIVE', volume_id,
+                                'STOP'], flag_force=True)
         except ExecError:
-            raise LsmError(ErrorNumber.PLUGIN_BUG, 'Volume-ident-led-off failed unexpectedly')
+            raise LsmError(ErrorNumber.PLUGIN_BUG,
+                           'Volume-ident-led-off failed unexpectedly')
 
         return None
