3c417db2c7091219b6c20ae584da13d430291a1c
Tony Asleson <tasleson@redhat.com>
2019-03-18 10:25:08 -0500

    Add simple integration test
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

---
+++ test/integration.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# We are wanting to execute the cmdtest and plugin test against
+# installed packages using the simulator.  This test ensures
+# that a packaged install is working.
+
+function _good
+{
+    echo "executing: $@"
+    eval "$@"
+    local rc=$?
+    if [ $rc -ne 0 ]; then
+        exit 1
+    fi
+}
+
+export LSMCLI_URI="sim://"
+_good python3 cmdtest.py.in -c /usr/bin/lsmcli
+
+export LSM_TEST_URI="sim://"
+_good python3 plugin_test.py.in
